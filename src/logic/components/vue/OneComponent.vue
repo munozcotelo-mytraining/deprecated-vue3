0d8f755763f482b77026bba68b0da97d6dd37253
Alvaro Muñoz Garcia <amgarcia@arsys.es>
2021-11-23 10:54:12 +0100

    Splict vue file

--- src/logic/components/vue/OneComponent.vue
+++ src/logic/components/vue/OneComponent.vue
@@ -2,7 +2,7 @@
 
     <div>
 
-        <p>One component</p>
+        <p>One component (2)</p>
 
         <p>{{ theName }}</p>
         <p>{{ theSurname }}</p>
@@ -14,62 +14,7 @@
 
 <script lang="ts">
 
-    import * as vue from "vue";
-
-    interface IBook {
-        name   : string;
-        author : string;
-    }
-
-    const OneComponent = vue.defineComponent( {
-
-        name: "OneComponent",
-
-        props : {
-
-            theName    : String,
-            theSurname : String,
-            theNumber  : Number,
-
-        },
-
-        data() {
-
-            return {
-
-                manolo : 50 as number,
-                book   : {
-
-                    name   : "Quijote",
-                    author : "Cervantes",
-
-                } as IBook,
-
-            }
-
-        },
-
-        beforeCreate () {
-            console.info( `En el hook de beforeCreate 1. Todavía no tengo this.book ${this.book}` );
-        },
-
-        created () {
-            console.info( `En el hook de created. Ya tengo this.book ${this.book}` );
-        },
-
-        mounted () {
-            console.info( "En el hook de mounted (12)" );
-            console.info( this.book.name );
-        },
-
-        beforeMount () {
-            console.info( "En el hook de beforeMount" );
-        },
-
-        // template: '<div>hola y adios</div>',
-
-    } );
-
+    import OneComponent from "./OneComponent";
     export default OneComponent;
 
 </script>
