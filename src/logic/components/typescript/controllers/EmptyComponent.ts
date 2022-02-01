ba3692081b9cb6ce2d8920e352558bd559af3386
Alvaro Muñoz Garcia <amgarcia@arsys.es>
2022-02-01 17:04:24 +0100

    Vue Router - Add & Remove routes dynamically

--- src/logic/components/typescript/controllers/EmptyComponent.ts
+++ src/logic/components/typescript/controllers/EmptyComponent.ts
@@ -1,20 +1,32 @@
 import * as vue       from "vue";
 import * as vueRouter from "vue-router";
 
+import { genRoutes } from "./../../../../../configuration/routing";
+
 interface IProps {
 }
 
 interface IData {
-    numero : number;
+
+    numero        : number;
+    routesCreated : boolean;
+
 }
 
 interface IComputed {
 }
 
 interface IMethod  {
+
+    removeRoutes : () => void;
+    addRoutes    : () => void;
 }
 
 interface IComponent extends IProps, IData, IMethod, IComputed {
+
+    $router : vueRouter.Router;
+    $route  : vueRouter.RouteLocationNormalized;
+
 }
 
 // const EmptyComponent : vue.DefineComponent = vue.defineComponent<{}, {}, {}, {}>( {
@@ -26,7 +38,10 @@ const EmptyComponent = vue.defineComponent( {
     data  () : IData {
 
         return {
-            numero : 4,
+
+            numero        : 4 as number,
+            routesCreated : true as boolean,
+
         };
 
     },
@@ -60,7 +75,36 @@ const EmptyComponent = vue.defineComponent( {
     created () : void {
 
         const me : IComponent = this;
-        console.info( "EmptyComponent", this.$route.meta );
+
+        console.info( "EmptyComponent", me.$route.meta );
+
+        console.info( me.$route );
+
+    },
+
+    methods : {
+
+        removeRoutes() : void {
+
+            const me : IComponent = this;
+            me.routesCreated      = false;
+
+            ( me.$router ).removeRoute( "route1" );
+            ( me.$router ).removeRoute( "route2" );
+            ( me.$router ).removeRoute( "empty" );
+
+        },
+
+        addRoutes() : void {
+
+            const me : IComponent = this;
+            me.routesCreated      = true;
+
+            genRoutes().forEach( ( route : vueRouter.RouteRecordRaw ) => {
+                ( me.$router ).addRoute( route );
+            } );
+
+        },
 
     },
 
--- src/logic/components/vue/controllers/EmptyComponent.vue
+++ src/logic/components/vue/controllers/EmptyComponent.vue
@@ -5,6 +5,15 @@
         <h2>Meta</h2>
         <p>{{ JSON.stringify( $route.meta ) }}</p>
         <p>Numero: {{ numero }}</p>
+
+        <br/>
+        <br/>
+        <p v-if="routesCreated===true">Click en las rutas funciona</p>
+        <p v-else>Click en las rutas NO funciona</p>
+        <br/>
+        <button v-on:click="removeRoutes()">Remove routes</button>
+        <br/>
+        <button v-on:click="addRoutes()">Add routes</button>
     </div>
 
 </template>
